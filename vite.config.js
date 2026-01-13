269792419c928f99e0e5d8acbf253b75ce883b6e
kemkengroger <kemkengroger@gmail.com>
2026-01-13 22:28:46 +0100

    modif

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import { defineConfig } from 'vite'
-import tailwindcss from 'tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
-   plugins: [
-    tailwindcss(),
-  ],
+   
   root: 'tp',
   build: {
     outDir: '../dist',
