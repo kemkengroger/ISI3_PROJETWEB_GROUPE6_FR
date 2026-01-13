de98d7e22fb0c692da93c0c5c8fcbb1c78c3dc91
kemkengroger <kemkengroger@gmail.com>
2026-01-13 22:44:13 +0100

    modif

--- tp/vite.config.ts
+++ tp/vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        connexion: resolve(__dirname, 'connexion.html'),
+        inscription: resolve(__dirname, 'inscription.html'),
+        // Ajoute TOUS tes fichiers HTML
+      }
+    }
+  }
 })
