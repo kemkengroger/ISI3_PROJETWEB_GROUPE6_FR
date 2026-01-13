428a01a3b780628a75b060131a83257f7c1a129a
kemkengroger <kemkengroger@gmail.com>
2026-01-13 23:49:13 +0100

    chemin

--- tp/vite.config.ts
+++ tp/vite.config.ts
@@ -16,14 +16,14 @@ export default defineConfig({
         boardEtudiant : resolve (__dirname, 'boardEtudiant.html'),
         tuteurIA : resolve(__dirname, 'tuteurIA.html'),
       
-        epreuve : resolve(__dirname, 'Ressouces/epreuve.html'),
-        c6e : resolve(__dirname, 'Ressouces/6e.html'),
-        c5e : resolve(__dirname, 'Ressouces/5e.html'),
-        c4e : resolve(__dirname, 'Ressouces/4e.html'),
-        c3e : resolve(__dirname, 'Ressouces/3e.html'),
-        c2nde : resolve(__dirname, 'Ressouces/2nde.html'),
-        c1ere : resolve(__dirname, 'Ressouces/1ere.html'),
-        terminale : resolve(__dirname, 'Ressouces/terminale.html'),
+        epreuve : resolve(__dirname, 'Ressources/epreuve.html'),
+        c6e : resolve(__dirname, 'Ressources/6e.html'),
+        c5e : resolve(__dirname, 'Ressources/5e.html'),
+        c4e : resolve(__dirname, 'Ressources/4e.html'),
+        c3e : resolve(__dirname, 'Ressources/3e.html'),
+        c2nde : resolve(__dirname, 'Ressources/2nde.html'),
+        c1ere : resolve(__dirname, 'Ressources/1ere.html'),
+        terminale : resolve(__dirname, 'Ressources/terminale.html'),
         discussionPeerToPeer : resolve(__dirname, 'Ressouces/discussionPeerToPeer.html'),
 
 
